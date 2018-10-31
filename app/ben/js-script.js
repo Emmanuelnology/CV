8d33e48cdb90381d71ce5ca840763c52b5e15d1c
Ben Tomaney <Ben.Tomaney@nology.io>
2018-10-31 20:19:33 +0000

    Used root in css

---
+++ app/ben/js-script.js
@@ -0,0 +1,37 @@
+var nav = {
+    cv: {
+        link: $("#nav-ben-cv"),
+        content:  $(".ben-cv"),
+    },
+    contact: {
+        link: $("#nav-contact"),
+        content:  $(".contact"),
+    },
+    about: {
+        link: $("#nav-about"),
+        content:  $(".about"),
+    },
+    skills: {
+        link: $("#nav-skills"),
+        content:  $(".skills"),
+    },
+    expereience: {
+        link: $("#nav-experience"),
+        content:  $(".experience"),
+    },
+    qualifications: {
+        link: $("#nav-qualifications"),
+        content:  $(".qualifications"),
+    },
+    hobbies: {
+        link: $("#nav-hobbies"),
+        content:  $(".hobbies"),
+    },
+    references: {
+        link: $("#nav-references"),
+        content:  $(".references"),
+    }
+}
+
+var allSections=$(".js-section");
+
