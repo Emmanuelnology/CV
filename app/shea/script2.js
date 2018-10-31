f0502a9f81071e0986dc2811c33e718620e4b5a5
Shea Murphy <Shea.Murphy@nology.io>
2018-10-31 14:47:50 +0000

    Created layout for mobile screen

---
+++ app/shea/script2.js
@@ -0,0 +1,60 @@
+var nav = {
+    contact: {
+        link: $("#js-contact"),
+        content:  $("#contact"),
+    },
+    skills: {
+        link: $("#js-skills"),
+        content:  $("#skills"),
+    },
+    employment: {
+        link: $("#js-employ"),
+        content:  $("#employment"),
+    },
+    education: {
+        link: $("#js-edu"),
+        content:  $("#education"),
+    },
+    awards: {
+        link: $("#js-awards"),
+        content:  $("#awards"),
+    },
+    recruiterInfo: {
+        link: $("#js-recruit"),
+        content:  $("#recruiter-info"),
+    }
+}
+
+var allSections=$(".js-section");
+
+for (var sectionName in nav) {
+    setNavClicks(nav[sectionName]);    
+}
+
+function setNavClicks(section) {
+    section.link.click(function(){
+        showSection(section);
+     });
+ }
+
+function removeActiveFromAllNavLinks(){
+    for (var sectionName in nav) {
+        nav[sectionName].link.removeClass('active');    
+     }
+}
+
+function showSection(section) {
+    allSections.hide();
+    removeActiveFromAllNavLinks();
+    section.content.show();
+    section.link.addClass('active');
+}
+
+$("#toggle1").click(function(){
+    $("#bspp").slideToggle();
+});
+
+$("#toggle2").click(function(){
+    $("#tefl").slideToggle();
+});
+
