e8ebfbad592fe3031f1194f2a7a2e066df382a9e
Luke Delaney <Luke.Delaney@nology.io>
2018-11-02 11:13:02 +0000

    modal start

---
+++ app/luke/pop-up.js
@@ -0,0 +1,9 @@
+$(".js-modal-close-ibm").click(function(){
+  $(".modal-ibm").hide(); 
+  $(".modal-ibm").fadeout();
+});
+
+$(".js-modal-open-ibm").click(function(){
+  $(".modal-ibm").show();
+  $(".modal-ibm").fadein();
+});
