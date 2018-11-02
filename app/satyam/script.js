1db53dc9b8c59a03cabecde6fbc2be2f6f1355ee
Satyam Gehani <Satyam.Gehani@nology.io>
2018-11-02 10:51:54 +0000

    attempt at making carousel work

--- app/satyam/script.js
+++ app/satyam/script.js
@@ -1,3 +1,9 @@
 $("#toggle").click(function(){
    $("#container").slideToggle();
 });
+
+$(document).ready(function(){
+    $('.image-carousel').slick({
+    setting-name: setting-value
+    });
+});
