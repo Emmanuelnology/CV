d50a14ac347c58b8fb0040be256cc58e7998bb97
Ben Tomaney <Ben.Tomaney@nology.io>
2018-11-06 23:01:38 +0000

    Moved trial over to main folder

---
+++ app/ben/trial/6-cell-grid.js
@@ -0,0 +1,14 @@
+function removeAllActiveClasses(){
+    $('#grid').removeClass('active-about');
+    $('#grid').removeClass('active-skills');
+    $('#grid').removeClass('active-work');
+    $('#grid').removeClass('active-contact');
+    $('#grid').removeClass('active-hobbies');
+    $('#grid').removeClass('active-qualifications');
+}
+
+$('#grid .grid-cell').click(function(){
+    removeAllActiveClasses();
+    var target=$(this).attr('data-target');
+    $("#grid").addClass(target);
+})
