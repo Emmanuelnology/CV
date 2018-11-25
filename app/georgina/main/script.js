d08e04e05c080e2f3bc2d06ebf97cee6f8c8ee5e
Georgina Ryder <Georgina.Ryder@nology.io>
2018-11-25 20:19:00 +0000

    contact me

--- app/georgina/main/script.js
+++ app/georgina/main/script.js
@@ -5,16 +5,25 @@ var nav = {
     },
     story: {
         link: $("#js-story"),
-        content: $("#story")
+        content: $("#story"),
+        hoverIn: function () {
+            $('#gee').attr('src',"gee.lips.png"); 
+        }
     },
     skills: {
         link: $("#js-skills"),
         content: $("#skills"),
-        div: $("#brain")
+        div: $("#brain"),
+        hoverIn: function () {
+            $('#gee').attr('src',"gee.brain.png"); 
+        }
     },
     aboutme: {
         link: $("#js-aboutme"),
-        content: $("#aboutme")
+        content: $("#aboutme"),
+        hoverIn: function () {
+            $('#gee').attr('src',"gee.heart.png"); 
+        }
     },
     hobbies: {
         link: $("#js-hobbies"),
@@ -54,4 +63,23 @@ $("#closebutton").click(function(){
 
 $(window).on('resize', function(){
     $("#main").show();
+});
+
+function hoverOut() {
+    $('#gee').attr('src',"gee.png"); 
+}
+
+$('#js-skills').hover(nav.skills.hoverIn,hoverOut);   
+$('#js-story').hover(nav.story.hoverIn,hoverOut);   
+$('#js-aboutme').hover(nav.aboutme.hoverIn,hoverOut);   
+
+
+$('#js-skills').click(function () {
+    $('#gee').attr('src',"gee.brain.png"); 
+});
+$('#js-story').click(function () {
+    $('#gee').attr('src',"gee.lips.png"); 
+});
+$('#js-aboutme').click(function () {
+    $('#gee').attr('src',"gee.heart.png"); 
 });
